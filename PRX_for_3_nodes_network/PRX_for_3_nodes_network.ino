2651630c933fcbc4f399a3ba1a7672e7cf1ef117
glitterbug <rishabhraul@gmail.com>
2015-08-08 08:44:01 +0530

    initial commit, working codes

---
+++ PRX_for_3_nodes_network/PRX_for_3_nodes_network.ino
@@ -0,0 +1,75 @@
+#include<SPI.h>
+#include<nRF24L01.h>
+#include<RF24.h>
+const uint64_t pipe[2] = { 0xB3B4B5B6CD , 0xB3B4B5B6F1};
+//uint8_t p[2];//={'1','2'};
+//p[0] = 0xB3B4B5B6CD & 0xfffff;
+//uint8_t pipe_num;
+//p[0] = 'CD'; p[1] = 'F1';
+//const uint64_t pipe[1] = {0xF0F0F0F0E1LL};
+RF24 radio(9, 10);
+int rec[1] = {2};
+char red[32];
+//byte p[2];
+uint8_t pipe_num;
+
+void setup()
+{
+  Serial.begin(57600);
+  radio.begin();
+  delay(100);
+  //radio.setAutoAck(true);
+  radio.setAutoAck(1,true);//delay(100);
+  radio.setAutoAck(2,true);
+  radio.enableAckPayload();//to enable CUSTOM payloads
+  radio.enableDynamicPayloads();// seems to be very necessary for sending out ack data to tx nodes.
+  radio.startListening();
+  radio.openReadingPipe(1, pipe[0]);
+  radio.openReadingPipe(2, pipe[1]);
+  radio.startListening();
+  radio.setRetries(15, 15);
+  Serial.println(pipe_num);
+}
+void loop() {//Serial.println(pipe_num);
+  //uint8_t pipe_num;
+  if ( radio.available(&pipe_num) ) {
+    if(pipe_num == 1)
+    {
+      //Serial.print("its one !!  ");
+      Serial.println(pipe_num);
+    }
+    else if(pipe_num == 2)
+    {
+      //Serial.print("its two  ");
+      Serial.println(pipe_num);
+    }
+      radio.writeAckPayload(pipe_num , rec, sizeof(int) );
+      radio.read( red, sizeof(red) );
+    rec[0] += 2;
+    //Serial.print("data got is : ");
+    Serial.println(red);
+    }
+  /*else if(radio.available(&pipe_num[1]))
+    {
+      Serial.println("its two !!");
+      radio.writeAckPayload(2, rec, sizeof(int) );
+      radio.read( red, sizeof(red) );
+    rec[0] += 2;
+    Serial.print("data got is : ");
+    Serial.println(red);
+    }
+    //radio.writeAckPayload(2 , rec, sizeof(int) );
+    //radio.writeAckPayload(2 , rec, sizeof(int) );
+    /*radio.read( red, sizeof(red) );
+    rec[0] += 2;
+    Serial.print("data got is : ");
+    Serial.println(red);*/
+  }
+  /*else if ( radio.available(pipe_num) ) {
+    radio.writeAckPayload( 2, rec, sizeof(int) );
+    radio.read( red, sizeof(red) );
+    rec[0] += 2;
+    Serial.print("character got is : ");
+    Serial.println(red);
+  }*/
+//}
---
+++ PTX_two/PTX_two.ino
@@ -0,0 +1,53 @@
+#include<SPI.h>
+#include<nRF24L01.h>
+#include<RF24.h>
+int rec[1] = {5};
+float time;
+bool go_to_tx_mode = false;
+char input[32] = "";
+RF24 radio(9, 10);
+const uint64_t pipe[1] = {0xB3B4B5B6CD};
+
+void setup()
+{
+  Serial.begin(57600);
+  radio.begin();
+  delay(100);
+  radio.setAutoAck(1,true);
+  radio.enableAckPayload();
+  radio.enableDynamicPayloads();
+  radio.stopListening();
+  radio.openWritingPipe(pipe[0]);
+  radio.setRetries(15, 15);
+}
+
+void loop() {
+
+  if (go_to_tx_mode) {
+    if (radio.write(input, sizeof(input)))
+    {
+      time = micros();
+      go_to_tx_mode = false;
+      //Serial.print( input );
+      //Serial.println("...tx success");
+      if (radio.isAckPayloadAvailable())
+      {
+        radio.read(rec, sizeof(int));
+        Serial.print(micros() - time);
+        Serial.print(".......received ack payload is : ");
+        Serial.println(rec[0]);
+      }
+      else
+      {
+        go_to_tx_mode = false; //doing this completely shuts down the transmitter if an ack payload is not received !!
+        Serial.println("status has become false so stop here....");
+      }
+    }
+  }
+  if (Serial.available())
+  {
+    int len = Serial.readBytesUntil('\n', input, 31);
+    input[len] = '\0';
+    go_to_tx_mode = true;
+  }
+}
